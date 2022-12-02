5a61033aea8dd9032bb2a60d5a0af0af260216ae
Duncan Beevers <duncan@dweebd.com>
2022-12-01 21:11:54 -0800

    feat: Try and connect to serial port

--- app/app.ts
+++ app/app.ts
@@ -1,11 +1,26 @@
 import express from 'express';
+import { SerialPort } from 'serialport';
+import { ReadlineParser } from '@serialport/parser-readline';
+
+const sport = new SerialPort({ path: '/dev/ttyAMA0', baudRate: 9600 })
+const parser = new ReadlineParser();
+sport.pipe(parser)
+
+const lines: string[] = [];
+
+parser.on('data', (line: string) => {
+  lines.push(line);
+  console.log(line);
+});
+
+sport.write('?\n');
 
 const app = express();
 
 const port = 3000;
 
 app.get("/", (_req, res) => {
-    res.send("Hi Dude");
+    res.send(`Hi Dude: ${JSON.stringify(lines, null, 2)}`);
 });
 
 app.listen(port, () => {
