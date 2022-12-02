c11dd56dcb87c0c0f18603d22b697d3e8d5bab60
Duncan Beevers <duncan@dweebd.com>
2022-12-01 19:10:11 -0800

    build: Build + run the express server

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,13 @@
-console.log('hello');
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (_req, res) => {
+    res.send("Hi Dude");
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
