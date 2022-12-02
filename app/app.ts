3b99d528fa0bc3d62be48ed9b5ec48d5d9298579
Duncan Beevers <duncan@dweebd.com>
2022-12-01 21:20:51 -0800

    feat: Export server function

--- app/app.ts
+++ app/app.ts
@@ -1,28 +1,30 @@
-import express from 'express';
-import { SerialPort } from 'serialport';
-import { ReadlineParser } from '@serialport/parser-readline';
+import express from "express";
+import { SerialPort } from "serialport";
+import { ReadlineParser } from "@serialport/parser-readline";
 
-const sport = new SerialPort({ path: '/dev/ttyAMA0', baudRate: 9600 })
-const parser = new ReadlineParser();
-sport.pipe(parser)
+export default function () {
+  const sport = new SerialPort({ path: "/dev/ttyAMA0", baudRate: 9600 });
+  const parser = new ReadlineParser();
+  sport.pipe(parser);
 
-const lines: string[] = [];
+  const lines: string[] = [];
 
-parser.on('data', (line: string) => {
-  lines.push(line);
-  console.log(line);
-});
+  parser.on("data", (line: string) => {
+    lines.push(line);
+    console.log(line);
+  });
 
-sport.write('?\n');
+  sport.write("?\n");
 
-const app = express();
+  const app = express();
 
-const port = 3000;
+  const port = 3000;
 
-app.get("/", (_req, res) => {
+  app.get("/", (_req, res) => {
     res.send(`Hi Dude: ${JSON.stringify(lines, null, 2)}`);
-});
+  });
 
-app.listen(port, () => {
+  app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
-});
+  });
+}
