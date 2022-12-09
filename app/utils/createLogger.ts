2d5944b0a2e0aad24dbbabdf58bbaf190d4bdf83
Duncan Beevers <duncan@dweebd.com>
2022-12-08 22:00:10 -0800

    chore: Add error and warn logger categories

--- app/utils/createLogger.ts
+++ app/utils/createLogger.ts
@@ -3,11 +3,17 @@ export default function createLogger(categoryName: string) {
     debug: (...args: any[]) => {
       return console.debug(`[${categoryName}]`, ...args);
     },
+    error: (...args: any[]) => {
+      return console.error(`[${categoryName}]`, ...args);
+    },
     info: (...args: any[]) => {
       return console.info(`[${categoryName}]`, ...args);
     },
     log: (...args: any[]) => {
       return console.log(`[${categoryName}]`, ...args);
+    },
+    warn: (...args: any[]) => {
+      return console.warn(`[${categoryName}]`, ...args);
     },
   };
 }
