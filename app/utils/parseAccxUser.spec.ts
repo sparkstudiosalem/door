d1c13057225fa40d8bd8b45ea8c83d5477ca94f9
Duncan Beevers <duncan@dweebd.com>
2022-12-08 22:13:17 -0800

    feat: Update ACCX user parsing

--- app/utils/parseAccxUser.spec.ts
+++ app/utils/parseAccxUser.spec.ts
@@ -6,27 +6,20 @@ describe("parseAccxUser", () => {
     ["0:0:0  1/1/0 SUN ", undefined],
     ["0:0:0  1/1/0 SUN User dump started.", undefined],
     [
-      "186	255	4294967295 ",
+      "186	255	102 ",
       {
         id: "186",
         userMask: 255,
-        tag: "4294967295",
+        tag: "102",
       },
     ],
+    ["197\t255\t4294967295", undefined],
     [
-      "197\t255\t4294967295",
-      {
-        id: "197",
-        userMask: 255,
-        tag: "4294967295",
-      },
-    ],
-    [
-      "189\t255\t4294967295",
+      "189\t255\t88",
       {
         id: "189",
         userMask: 255,
-        tag: "4294967295",
+        tag: "88",
       },
     ],
     ["UserNum: Usermask: TagNum:", undefined],
