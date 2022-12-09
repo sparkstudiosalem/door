7b03c4ad07b62c1e8d4860bd4a0645331312d20d
Duncan Beevers <duncan@dweebd.com>
2022-12-08 18:54:04 -0800

    feat: Improve ACCX Date format parsing

--- app/utils/parseAccxDate.spec.ts
+++ app/utils/parseAccxDate.spec.ts
@@ -1,10 +1,10 @@
 import parseAccxDate from "./parseAccxDate";
 
 describe("parseAccxDate", () => {
-  test.each([["0:0:0  1/1/0 SUN \r", new Date("2000-01-01T00:00:00.000Z")]])(
+  test.each([["0:0:0  1/1/0 SUN \r", "2000-01-01T00:00:00.000Z"]])(
     "accxDate: %s date: %o",
-    (accxDate, date) => {
-      expect(parseAccxDate(accxDate).toISOString()).toEqual(date.toISOString());
+    (accxDate, dateString) => {
+      expect(parseAccxDate(accxDate).toISOString()).toBe(dateString);
     }
   );
 });
