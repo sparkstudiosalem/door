abe62439f677728dd643973ce18d23ebb4a0a8c8
Duncan Beevers <duncan@dweebd.com>
2022-12-08 22:00:37 -0800

    feat: Log parseAccxUser states

--- app/utils/parseAccxUser.ts
+++ app/utils/parseAccxUser.ts
@@ -1,4 +1,12 @@
 import { components } from "../generated/schema/openapi";
+import createLogger from "./createLogger";
+
+const log = createLogger(__filename);
+
+// When a user is removed from the roster the slot is maintained, and the
+// tag is set to this default value.
+// https://en.wikipedia.org/wiki/4,294,967,295
+const UNSET_TAG = "4294967295"; // FFFF_FFFF
 
 export default function parseAccxUser(
   accxUserString: string
@@ -6,25 +14,32 @@ export default function parseAccxUser(
   const userParts = accxUserString.split(/\s+/);
 
   if (userParts.length < 3) {
-    // throw new Error(
-    //   `Unrecognized ACCX user format ${JSON.stringify(
-    //     accxUserString
-    //   )}; overall format`
-    // );
+    log.error(
+      `Unrecognized ACCX user format ${JSON.stringify(
+        accxUserString
+      )}; overall format`
+    );
     return undefined;
   }
 
   const [id, userMask, tag] = userParts;
   if (!id || !userMask || !tag) {
-    // throw new Error(
-    //   `Unrecognized ACCX user format ${JSON.stringify(
-    //     accxUserString
-    //   )}; missing id, userMask, or tag`
-    // );
+    log.error(
+      `Unrecognized ACCX user format ${JSON.stringify(
+        accxUserString
+      )}; missing id, userMask, or tag`
+    );
+
+    return undefined;
+  }
+
+  if (tag === UNSET_TAG) {
+    log.info(`Ignoring tag with default value 0xFFFF_FFFF ${tag}`);
     return undefined;
   }
 
   if (id.includes(":")) {
+    log.info(`Ignoring id including colon ${id}`);
     return undefined;
   }
 
