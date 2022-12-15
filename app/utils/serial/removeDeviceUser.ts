35edc486296f69d157130c21681c567d7a034fcf
Duncan Beevers <duncan@dweebd.com>
2022-12-15 13:20:02 -0800

    feat: removeDeviceUser is privileged action

--- app/utils/serial/removeDeviceUser.ts
+++ app/utils/serial/removeDeviceUser.ts
@@ -5,6 +5,7 @@ import { REMOVE_USER } from "./constants";
 export default async function removeDeviceUser(userId: string) {
   return runSession({
     command: REMOVE_USER,
+    isPrivileged: true,
     params: [userId],
     onData: (onComplete: (result: Boolean) => void, data: string) => {
       const nextUser = parseAccxUser(data);
